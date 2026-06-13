8eed457914c1454f085f10413c8806b7c9f867a8
Iyanuloluwa Okinbaloye <iyanuokinbaloye@gmail.com>
2026-06-13 21:20:05 +0100

    use wsgi.py entry point for gunicorn

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'web_demo'))
+from app import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
